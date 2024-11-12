7bfbe6ca252965f10a2feffd8f00af89b2e24fe3
CodigoWaldo <70487661+CodigoWaldo@users.noreply.github.com>
2024-11-12 12:25:26 -0300

    añadido datapath

---
+++ Datapath/datapath.v
@@ -0,0 +1,17 @@
+/*
+Camino de datos: Conexiones que relacionan los distintos 
+modulos para conformar el procesador.
+*/
+
+
+module datapath(
+   
+);
+
+
+always @(*) 
+    begin  
+      
+    end
+
+endmodule
